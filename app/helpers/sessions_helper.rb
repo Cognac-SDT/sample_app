28665b4d0a16f7d79d86b54f315e451df9eb95f4
Cognac-SDT <hovansang2512@gmail.com>
2018-11-21 21:09:48 +0700

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,46 @@
 module SessionsHelper
-
-  # Logs in the given user.
+  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
-
-  # Returns the current logged-in user (if any).
+  
+  # ユーザーを永続的に復元できるようになった
+  def remember(user)
+    user.remember # => DB: remember_digest
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+
+    elsif (user_id = cookies.signed[:user_id])
+      # raise
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
-  # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
+  
+   # 現在のユーザーをログアウトする
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  
 end
